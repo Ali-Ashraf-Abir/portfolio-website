97deb7eb3be6b3578de4d02fc00f707fc1d9bdd6
Ali-Ashraf-Abir <fazilabir88@gmail.com>
2023-03-27 19:30:28 +0600

    finished initial stage

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+let card1stat=''
+
+
 function intro(){
 
     tl=gsap.timeline()
@@ -7,6 +10,7 @@ function intro(){
     tl4=gsap.timeline()
     tl5=gsap.timeline()
     tl6=gsap.timeline()
+    tl7=gsap.timeline()
     tl.from('.intro',{duration:1,top:-50,stagger:0.25,ease:'elastic'})
     tl.to('.intro-screen',{delay:2,duration:1,clipPath: 'inset(50% 0 50% 0)',display:'hidden'})
     tl2.from('.card1',{delay:5,duration:0.5,top:380,left:700,ease:'ease-in'})
@@ -15,7 +19,12 @@ function intro(){
     tl2.from('.card4',{duration:0.5,top:300,left:300,ease:'ease-in'})
     tl2.from('.select-card',{duration:0.5,clipPath:'inset(0 0 0 100%)',ease:'ease-in'})
 
+
+
+
+
     document.querySelector('.card1-svg').addEventListener('mouseenter',function(){
+        if(card1stat!='clicked'){
         tl3.from('.card1-info',{duration:0.5,display:'hidden',clipPath: 'inset(0 0 0 0)',})
         tl3.to('.card1-svg',{duration:0.5,clipPath: 'inset(50% 0 50% 0)',display:'hidden'})
         tl3.from('.card1-info',{duration:0.5,display:'block',clipPath: 'inset(100% 0 0 0)',})
@@ -24,7 +33,7 @@ function intro(){
         tl3.to('.card1-svg',{delay:5,duration:0.5,clipPath: 'inset(0 0 0 0)',display:'block'})
         tl3.to('.card1-info',{duration:0.5,display:'hidden',clipPath: 'inset(100% 0 0 0)',})
       
-
+        }
 
     })
     document.querySelector('.card2-svg').addEventListener('mouseenter',function(){
@@ -64,3 +73,57 @@ function intro(){
 }
 
 intro()
+
+
+
+
+function functionality(){
+
+    
+    let card1=document.querySelector('.card1')
+    let card2=document.querySelector('.card2')
+    let card3=document.querySelector('.card3')
+    let card4=document.querySelector('.card4')
+    let card1Content=document.getElementById('card-content')
+    let card1Svg=document.querySelector('.card1-svg')
+    let card1Info=document.querySelector('.card1-info')
+
+    card1.addEventListener('click',function(){
+        console.log('clicked')
+        card1stat='clicked'
+        
+        gsap.to('.card1-content',{duration:1,height:'100vh',width:'100%',top:0,left:0,zIndex:'1500'})
+
+
+
+    })
+    card2.addEventListener('click',function(){
+        console.log('clicked')
+        card1stat='clicked'
+        
+        gsap.to('.card2-content',{duration:1,height:'100vh',width:'100%',top:0,left:0,zIndex:'1500'})
+
+
+
+    })
+    card3.addEventListener('click',function(){
+        console.log('clicked')
+        card1stat='clicked'
+        
+        gsap.to('.card3-content',{duration:1,height:'100vh',width:'100%',top:0,left:0,zIndex:'1500'})
+
+
+
+    })
+    card4.addEventListener('click',function(){
+        console.log('clicked')
+        card1stat='clicked'
+        
+        gsap.to('.card4-content',{duration:1,height:'100vh',width:'100%',top:0,left:0,zIndex:'1500'})
+
+
+
+    })
+}
+
+functionality()
